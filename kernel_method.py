68027c33d09e33e7e3784f8faef699a36a022492
Louis-Félix Vigneux <lf@MacBook-Pro-de-Louis-Felix.local>
2024-09-06 14:22:07 -0400

    Little test

--- kernel_method.py
+++ kernel_method.py
@@ -4,3 +4,7 @@ import numpy as np
 from matplotlib import cm
 from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
+
+
+def get_pulsar_dataset():
+    pass
