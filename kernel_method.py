c7967767b74f335c1e0b99bf122d850823e43158
Louis-Félix Vigneux <lf@MacBook-Pro-de-Louis-Felix.local>
2024-09-06 14:20:42 -0400

    Created a base file with some basic imports

---
+++ kernel_method.py
@@ -0,0 +1,6 @@
+import pennylane as qml
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
