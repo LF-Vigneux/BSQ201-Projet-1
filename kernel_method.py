26dc2e29c75c20f1e507b6127defb6ba7802d956
Louis-Félix Vigneux <lf@MacBook-Pro-de-Louis-Felix.local>
2024-09-06 14:54:46 -0400

    Reordered the files

--- kernel_method.py
+++ kernel_method.py
@@ -8,22 +8,5 @@ from typing import Tuple
 from numpy.typing import NDArray
 
 
-def get_feature_vectors_and_labels(
-    path: str, dataset_name: str
-) -> Tuple[NDArray[np.float_], NDArray[np.float_]]:
-    """
-    Reads the datasets from a file and divides the data into two matrices. The first one is the
-    feature vectors and the othe rone is the labels. The labels of the dataset to read must be
-    in the last column.
-
-    Parameters:
-    - path (str): The relative path to the dataset file.
-    - dataset_name (str): The name of the datasdet file including the extension.
-
-    Returns:
-    Tuple[NDArray[np.float_], NDArray[np.float_]]:  - The matrix of the feature vectors
-                                                    - The vector of the set's labels
-    """
-    dataset = np.load(path + dataset_name, allow_pickle=True)
-
-    return dataset[:, :-1], dataset[:, -1]
+def embedding_circuit():
+    pass
---
+++ utils.py
@@ -0,0 +1,28 @@
+import pennylane as qml
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
+from typing import Tuple
+from numpy.typing import NDArray
+
+
+def get_feature_vectors_and_labels(
+    path: str, dataset_name: str
+) -> Tuple[NDArray[np.float_], NDArray[np.float_]]:
+    """
+    Reads the datasets from a file and divides the data into two matrices. The first one is the
+    feature vectors and the othe rone is the labels. The labels of the dataset to read must be
+    in the last column.
+
+    Parameters:
+    - path (str): The relative path to the dataset file.
+    - dataset_name (str): The name of the datasdet file including the extension.
+
+    Returns:
+    Tuple[NDArray[np.float_], NDArray[np.float_]]:  - The matrix of the feature vectors
+                                                    - The vector of the set's labels
+    """
+    dataset = np.load(path + dataset_name, allow_pickle=True)
+    return dataset[:, :-1], dataset[:, -1]
