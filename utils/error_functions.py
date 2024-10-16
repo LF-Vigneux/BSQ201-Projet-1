ed4a8bee15da784012f8554b7c6c02f5b162c1a0
Louis-Félix Vigneux <lf@MacBook-Pro-de-Louis-Felix.local>
2024-10-16 16:29:25 -0400

    Datsets

--- utils/error_functions.py
+++ utils/error_functions.py
@@ -6,7 +6,7 @@ from statistics import stdev
 
 def mean_square_error(
     predicted_labels: NDArray[np.float_], expirement_labels: NDArray[np.float_]
-) -> float:
+):
     total = 0
     for true_value, predicted in zip(predicted_labels, expirement_labels):
         total += (predicted - true_value) ** 2
@@ -16,7 +16,7 @@ def mean_square_error(
 # Les deux prochaines viennent de mon stage mais pourraient être interessantes à tester
 def normalized_mean_square_error(
     predicted_labels: NDArray[np.float_], expirement_labels: NDArray[np.float_]
-) -> float:
+):
     total_num = 0
     total_denom = 0
     for true_value, predicted in zip(predicted_labels, expirement_labels):
@@ -27,14 +27,14 @@ def normalized_mean_square_error(
 
 def normalized_root_mean_square_error(
     predicted_labels: NDArray[np.float_], expirement_labels: NDArray[np.float_]
-) -> float:
+):
     total = 0
     for true_value, predicted in zip(predicted_labels, expirement_labels):
         total += (predicted - true_value) ** 2
     return np.sqrt((1 / len(predicted_labels)) * total) / stdev(predicted_labels)
 
 
-# Ces errors functions sont inspirées du papier de pulsar
+# These functions are inspired by the pulsar paper
 def accuracy(
     predicted_labels: NDArray[np.float_], expirement_labels: NDArray[np.float_]
 ) -> float:
@@ -85,9 +85,6 @@ def negative_prediction_value(
         predicted_labels, expirement_labels
     )
     return true_negative / (true_negative + false_negative)
-
-
-# Peut-être ceux-là optimiser les autres fonctions pour appeler juste une fois la boucle des true positive/negative
 
 
 def balanced_accuracy(
